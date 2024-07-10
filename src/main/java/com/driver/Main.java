27bd400ddde87fbb757482c8c0dbba54586d4fbd
Rakesh Munukuntla <rakesh9490434912@gmail.com>
2024-07-10 12:40:33 +0530

    god grace

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,24 +2,24 @@ package com.driver;
 
 public class Main
 {
+    static class Product
+    {
+        public int product(int x, int y) {
+            return x+y;
+        }
+        public int product(int x, int y, int z)
+        {
+            return x+y+z;
+        }
+        public double product(double x, double y)
+        {
+            return x+y;
+        }
+    }
     public static void main(String[] args) {
-        Product p=new Product();
+        Product p= new Product();
         p.product(10,20);
         p.product(10,20,30);
         p.product(10.50,20.25);
-    }
-}
-class Product
-{
-    public int product(int x, int y) {
-        return x+y;
-    }
-    public int product(int x, int y, int z)
-    {
-        return x+y+z;
-    }
-    public double product(double x, double y)
-    {
-        return x+y;
     }
 }
